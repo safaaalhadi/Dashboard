744bec9f37d6124fc38c387a90cddcb364436388
Nour99988 <Noornoor19932011@gmail.com>
2022-07-03 22:47:28 +0300

    v1.21

--- src/components/User/ViewUser.js
+++ src/components/User/ViewUser.js
@@ -1,59 +1,85 @@
-import React,{useState,useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { Row } from "reactstrap";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
+export default function ViewUser() {
+  const { id } = useParams();
+  const [data, setData] = useState({});
+  const state = useSelector((state) => state.allMembers);
+  const member = state.find((e) => e.memberId === id);
+  //   setData(member);
+  useEffect(() => {
+    setData(member);
+    console.log(member);
+  }, [state]);
+  // function Handlerender(){
+  //     const member = state.filter((e)=> e.memberId === id);
 
+  // }
 
-export default function ViewUser(){
-    const {id} = useParams();
-    const [data,setData] = useState([]);
-    const state = useSelector(state => state.allMembers);
-    // const member = state.filter((e)=> e.memberId === id);
-    // console.log(member);
-    // useEffect(()=>{
-    //     setData(member);
-    // },[member])
-    // function Handlerender(){
-    //     const member = state.filter((e)=> e.memberId === id);
-
-    // }
-    const render = data.map((user)=>{
-        return(
-            <div className="col-sm-4 mb-1 mt-40" key={user.memberId} >
-            <div  className="card view  p-3">
-                <p>Name: {user.firstName} {user.lastName}</p>
-               <p> <b style={{color:'#eb8324'}}>memberId</b> :{user.memberId}</p>
-               <p> <b style={{color:'#eb8324'}}>generatedId </b>:{user.generatedId}</p>
-               <p><b style={{color:'#eb8324'}}>specification </b>  :{user.specification}</p>
-               <p><b style={{color:'#eb8324'}}>duration </b>:{user.duration}</p>
-               <p><b style={{color:'#eb8324'}}>projects</b> :{user.projects}</p>
-               <p><b style={{color:'#eb8324'}}>address</b> :{user.address}</p>
-               <p><b style={{color:'#eb8324'}}> kpi</b> :{user.kpi}</p>
-               <p><b style={{color:'#eb8324'}}>hardSkills</b> :{user.hardSkills}</p>
-               <p><b style={{color:'#eb8324'}}>softSkills </b> :{user.softSkills}</p>
-               <p><b style={{color:'#eb8324'}}>supervisor</b> :{user.supervisor}</p>
-               <p><b style={{color:'#eb8324'}}>startDate </b>:{user.startDate}</p>
-               <p><b style={{color:'#eb8324'}}>endDate </b>:{user.endDate}</p> 
-               <p><b style={{color:'#eb8324'}}>isIntern </b>:{user.isIntern}</p> 
-               <p><b style={{color:'#eb8324'}}>isEmployee </b>:{user.isEmployee}</p> 
-            
-
+  return (
+    <>
+      {!data ? (
+        <CircularProgress color="inherit" />
+      ) : (
+        <div className="content" style={{ paddingLeft: "30px" }}>
+          <Row className="m-0">
+            <div className="col-sm-4 mb-1 mt-40" key={data.memberId}>
+              <div className="card view  p-3">
+                <p>
+                  Name: {data.firstName} {data.lastName}
+                </p>
+                <p>
+                  {" "}
+                  <b style={{ color: "#eb8324" }}>memberId</b> :{data.memberId}
+                </p>
+                <p>
+                  {" "}
+                  <b style={{ color: "#eb8324" }}>generatedId </b>:{data.generatedId}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}>specification </b> :{data.specification}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}>duration </b>:{data.duration}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}>projects</b> :{data.projects}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}>address</b> :{data.address}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}> kpi</b> :{data.kpi}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}>hardSkills</b> :{data.hardSkills}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}>softSkills </b> :{data.softSkills}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}>supervisor</b> :{data.supervisor}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}>startDate </b>:{data.startDate}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}>endDate </b>:{data.endDate}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}>isIntern </b>:{data.isIntern}
+                </p>
+                <p>
+                  <b style={{ color: "#eb8324" }}>isEmployee </b>:{data.isEmployee}
+                </p>
+              </div>
             </div>
+          </Row>
         </div>
-        )
-    })
-    return(
-        <>
-            {
-           data.length <1 ? <CircularProgress color="inherit"/> : <div className="content" style={{paddingLeft:'30px' }}>
-              <Row className="m-0">
-              {render}
-              </Row>
-          </div>
-          }
-
-        </>
-    )
+      )}
+    </>
+  );
 }
