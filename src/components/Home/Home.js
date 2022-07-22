3a3a88763221371c35dc258ec51c27804fec8be1
safaaalhadi <safaaalhadisb@gmail.com>
2022-07-22 03:07:03 +0300

    dsss

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,36 @@
 import React, { useState } from "react";
 import style from "./Home.module.css";
-import { useSelector } from "react-redux";
 import Footer from "../Footer/Footer";
+import axios from "axios";
 
 const Home = () => {
-  const [filter , setFilter] = useState([]);
-  const [search, setSearch] = useState('');
-  const state = useSelector(state => state.allMembers);
-console.log(state);
-   
-    const handleChange = (text) =>{
-      let method=[];
-      if(text.length > 0){
-        method = state.filter((users)=>{
-          const value = new RegExp(`${search}`,'gi');
-          return users.firstName.match(value) || users.lastName.match(value) || users.memberId.match(value);
-        })
-      }
-      setFilter(method);
-      setSearch(text);
-    }
-    const onFilterHandle = (text)=>{
-      setSearch(text);
-      setFilter([]);
+
+  const [url, setUrl] = useState("");
+  const [state,setState] = useState({});
+
   
+    const handleChange = (text) =>{
+     const type = text.includes(" ");
+     if (type === true){
+      const arr = text.split(" ")
+      setUrl(`member?firstName=${arr[0]}&lastName=${arr[1]}`)
+     }else{
+      setUrl(text)
     }
+    console.log(url);
+       
+    }
+    const fetch = async()=>{
+      await axios.get(`https://focalx-cert-generator.herokuapp.com/v1/members/${url}`)
+      .then((res)=>{
+        console.log(res);
+        setState(res.data)
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+    }
+    
   return ( 
   <div className={style.contenar}>
     <div className={style.content}>
@@ -37,23 +43,12 @@ console.log(state);
       <p className={style.paragraph}>Search By Name Or Certificate ID Number</p>
       <div>
         <div className={style.input}>
-          <input type="search" placeholder="Search For Employee's Or Intern's" value={search} onChange={(e) => handleChange(e.target.value)} className="input" />
+          <input type="search" placeholder="Search For Employee's Or Intern's"  onChange={(e) => handleChange(e.target.value)} className="input" />
           <p>|</p>
-          <span><img src={require('../images/search.svg').default}/></span>
+          <span onClick={fetch} className={style.search}><img src={require('../images/search.svg').default} alt="safaa"/></span>
         </div>
        
-        <div>
-          {filter &&
-            filter.map((e, i) => (
-              <>
-                <div className={style.Filt}>
-                  <div className={style.filter} onClick={() => onFilterHandle(e.firstName)} key={i}>
-                    {e.firstName} {e.lastName}
-                  </div>
-                </div>
-              </>
-            ))}
-        </div>
+      
       </div>
       </div>
       <Footer />
